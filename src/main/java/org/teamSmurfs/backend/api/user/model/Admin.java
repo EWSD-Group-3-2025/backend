8d78e5c5d5d5d04d56264dbff9c673df5dc130ac
thanthtooaung-coding <thanthtoo1285@gmail.com>
2025-02-12 17:06:02 +0700

    [add & fix] Add Admin, Staff, Student, Tutor and Allocation entities and fix User entity

---
+++ src/main/java/org/teamSmurfs/backend/api/user/model/Admin.java
@@ -0,0 +1,24 @@
+package org.teamSmurfs.backend.api.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = true)
+    private String permissions;
+}
---
+++ src/main/java/org/teamSmurfs/backend/api/user/model/Tutor.java
@@ -0,0 +1,31 @@
+package org.teamSmurfs.backend.api.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.teamSmurfs.backend.api.allocation.model.Allocation;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tutor")
+public class Tutor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = true)
+    private String specialization;
+
+    @OneToMany(mappedBy = "tutor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Allocation> allocations;
+
+}
