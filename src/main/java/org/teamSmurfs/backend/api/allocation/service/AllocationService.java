a5c2f9e33f3921deb07b1744be56e336683286da
thanthtooaung-coding <thanthtoo1285@gmail.com>
2025-02-15 15:10:47 +0700

    [add] Add Allocate, Reallocate and Bulk Allocation features

--- src/main/java/org/teamSmurfs/backend/api/allocation/service/AllocationService.java
+++ src/main/java/org/teamSmurfs/backend/api/allocation/service/AllocationService.java
@@ -1,8 +1,13 @@
 package org.teamSmurfs.backend.api.allocation.service;
 
 import org.teamSmurfs.backend.api.allocation.dto.AllocationDto;
+import org.teamSmurfs.backend.api.allocation.dto.BulkCreateAllocationRequest;
+import org.teamSmurfs.backend.api.allocation.dto.CreateAllocationRequest;
+
+import java.util.List;
 
 public interface AllocationService {
-    AllocationDto allocate();
-    AllocationDto reallocate();
+    AllocationDto allocate(CreateAllocationRequest request);
+    AllocationDto reallocate(CreateAllocationRequest request);
+    List<AllocationDto> bulkAllocate(BulkCreateAllocationRequest request);
 }
