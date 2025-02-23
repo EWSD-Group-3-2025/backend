850905b01c9b23186971a43ef332b72d8f3ec8be
Thant Htoo Aung <thanthtoo1285@gmail.com>
2025-02-23 13:15:01 +0700

    [fix] Finish up allocation API

--- src/main/java/org/teamSmurfs/backend/api/allocation/dto/CreateAllocationRequest.java
+++ src/main/java/org/teamSmurfs/backend/api/allocation/dto/CreateAllocationRequest.java
@@ -1,15 +1,18 @@
 package org.teamSmurfs.backend.api.allocation.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Builder
 public class CreateAllocationRequest {
 
-    @NotNull(message = "Student ID is required")
-    private Long studentId;
+    @NotEmpty(message = "Student IDs list cannot be empty")
+    private List<Long> studentIds;
 
     @NotNull(message = "Tutor ID is required")
     private Long tutorId;
--- src/main/java/org/teamSmurfs/backend/api/allocation/repository/AllocationRepository.java
+++ src/main/java/org/teamSmurfs/backend/api/allocation/repository/AllocationRepository.java
@@ -2,6 +2,10 @@ package org.teamSmurfs.backend.api.allocation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.teamSmurfs.backend.api.allocation.model.Allocation;
+import org.teamSmurfs.backend.api.user.model.Student;
+
+import java.util.Optional;
 
 public interface AllocationRepository extends JpaRepository<Allocation, Long> {
+    Optional<Allocation> findByStudentAndActiveTrue(Student student);
 }
