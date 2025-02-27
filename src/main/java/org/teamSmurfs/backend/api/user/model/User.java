d4590c96f1ce136bf9e34f9c7f7939b195c5ddec
htet-hlyan123 <hlyanhtet127@gmail.com>
2025-02-28 02:01:05 +0630

    scheduler mail modified

--- src/main/java/org/teamSmurfs/backend/api/user/model/User.java
+++ src/main/java/org/teamSmurfs/backend/api/user/model/User.java
@@ -64,6 +64,9 @@ public class User {
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Tutor tutor;
 
+    @Column(name = "inactive", nullable = false)
+    private boolean inactive = true;
+
     @CreationTimestamp
     private LocalDateTime createdAt;
 
