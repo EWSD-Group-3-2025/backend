790dcaa3cc09a6379a44a1811c48d3b4943528ff
Thant Htoo Aung <thanthtoo1285@gmail.com>
2025-03-09 11:22:16 +0700

    [fix] Fix handleReaction with Update React function

--- src/main/java/org/teamSmurfs/backend/api/react/repository/ReactJdbcRepository.java
+++ src/main/java/org/teamSmurfs/backend/api/react/repository/ReactJdbcRepository.java
@@ -8,4 +8,5 @@ public interface ReactJdbcRepository {
     List<ReactRecord> findByEntityIdAndEntityType(final Long entityId, final Integer entityType);
     void giveReaction(final Long authorId, final String reaction, final Long entityId, final Integer entityType);
     void undoReaction(final Long authorId, final Long entityId, final Integer entityType);
+    void updateReaction(final Long authorId, final String reaction, final Long entityId, final Integer entityType);
 }
